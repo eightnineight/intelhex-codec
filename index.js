780d3f5d367f78ac17aaf68cbc8735e7597bb807
chihhsin hsu <eightnineight@gmail.com>
2024-02-27 14:34:43 +0800

    [phase]

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const fromString = (data) => {
+
+}
+const asString = (data) => {
+
+}
+
+const intelhexCodec = {
+    encode: {
+        asString,
+    },
+    decode: {
+        fromString,
+    },
+};
+
+export {
+    intelhexCodec,
+};
