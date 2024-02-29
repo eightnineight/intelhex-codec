f13ac6b7fff69854677cd616b2b5151f57d8997e
chihhsin hsu <eightnineight@gmail.com>
2024-02-29 18:06:53 +0800

    [add] encode

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { sprintf } from 'sprintf-js';
-
 const isDigit = (ch) => {
     return (0x30 <= ch && ch <= 0x39);
 }
@@ -17,7 +15,7 @@ const isAlphaDigit = (ch) => {
 }
 
 const byte2hex = (byte) => {
-    return [((byte >> 4) & 0x0f).toString(16), (byte & 0x0f).toString(16)];
+    return [((byte >> 4) & 0x0f).toString(16).toUpperCase(), (byte & 0x0f).toString(16).toUpperCase()];
 }
 const nibble2byte = (nibble) => {
     if (isDigit(nibble)) {
@@ -36,8 +34,98 @@ const hex2byte = (hex) => {
 
 class intelhexCodec {
     static encode = class {
-        static asString = (input) => {
-            
+        static #record2Line = (record) => {
+            if (!record.data) {
+                record.data = [];
+            }
+            const line = [':', ...byte2hex(record.data.length), ...byte2hex(record.offset >> 8), ...byte2hex(record.offset), ...byte2hex(record.type)];
+
+            let checksum = 0;
+            checksum += record.data.length;
+            checksum += (record.offset >> 8) & 0xff;
+            checksum += (record.offset) & 0xff;
+            checksum += record.type;
+
+            for (let data of record.data) {
+                line.push(...byte2hex(data));
+                checksum += data;
+            }
+            line.push(...byte2hex((0x100 - (checksum & 0xff)) & 0xff));
+            line.push('\r', '\n');
+
+            return line;
+        }
+        static #block2Lines = (block, byteCount = 16) => {
+            if (!block?.data?.length) {
+                return [];
+            }
+
+            const lines = [];
+
+            if (byteCount > 255) {
+                byteCount = 255;
+            }
+
+            lines.push(this.#record2Line({
+                offset: 0,
+                type: 4,
+                data: [(block.address >> 24) & 0xff, (block.address >> 16) & 0xff],
+            }));
+
+            let offset = block.address & 0xffff;
+            while (block?.data?.length) {
+                let len = byteCount;
+                if (block.data.length < byteCount) {
+                    len = block.data.length;
+                }
+                lines.push(this.#record2Line({
+                    offset,
+                    type: 0,
+                    data: block.data.splice(0, len),
+                }));
+                offset += len;
+            }
+
+            return lines;
+        }
+
+        static asString = (input, byteCount = 16) => {
+            const blocks = [];
+            for (let block of input) {
+                if (block.data.length < 65536) {
+                    blocks.push({
+                        address: block.address,
+                        data: [...block.data],
+                    });
+                } else {
+                    blocks.push({
+                        address: block.address,
+                        data: block.slice(0, 65536),
+                    });
+                    block.push({
+                        address: block.address + 65536,
+                        data: block.slice(65536),
+                    });
+                }
+            }
+
+            const lines = [];
+
+            for (let block of blocks) {
+                lines.push(...this.#block2Lines(block, byteCount));
+            }
+            lines.push(this.#record2Line({
+                offset: 0,
+                type: 1,
+                data: [],
+            }))
+
+            const hexString = [];
+            for (let line of lines) {
+                hexString.push(...line);
+            }
+
+            return hexString;
         };
     };
 
@@ -68,7 +156,7 @@ class intelhexCodec {
             }
             return lines;
         }
-        static #splitLine2fields = (line) => {
+        static #splitLine2Records = (line) => {
             if (line[0] !== ':'.charCodeAt(0)) {
                 throw 'ERR_WRONG_FORMAT';
             }
@@ -78,7 +166,7 @@ class intelhexCodec {
                 throw 'ERR_WRONG_FORMAT';
             }
 
-            const fields = {};
+            const records = {};
 
             let byte = 0;
             let sum = 0;
@@ -95,25 +183,25 @@ class intelhexCodec {
 
             byte = hex2byte(line);
             sum += byte;
-            fields.address = byte << 8;
+            records.address = byte << 8;
             line.splice(0, 2);
 
             byte = hex2byte(line);
             sum += byte;
-            fields.address += byte;
+            records.address += byte;
             line.splice(0, 2);
 
             byte = hex2byte(line);
             sum += byte;
-            fields.recordType = byte;
+            records.type = byte;
             line.splice(0, 2);
 
-            fields.data = [];
+            records.data = [];
 
             while (line.length > 2) {
                 let byte = hex2byte(line);
                 sum += byte;
-                fields.data.push(byte);
+                records.data.push(byte);
                 line.splice(0, 2);
             }
 
@@ -122,46 +210,48 @@ class intelhexCodec {
                 throw 'ERR_WRONG_FORMAT';
             }
 
-            switch (fields.recordType) {
+            switch (records.type) {
                 case 2: {
-                    if (fields.data.length !== 2) {
+                    if (records.data.length !== 2) {
                         throw 'ERR_WRONG_FORMAT';
                     }
                 } break;
 
                 case 3: {
-                    if (fields.data.length !== 4) {
+                    if (records.data.length !== 4) {
                         throw 'ERR_WRONG_FORMAT';
                     }
                 } break;
 
                 case 4: {
-                    if (fields.data.length !== 2) {
+                    if (records.data.length !== 2) {
                         throw 'ERR_WRONG_FORMAT';
                     }
                 } break;
 
                 case 5: {
-                    if (fields.data.length !== 4) {
+                    if (records.data.length !== 4) {
                         throw 'ERR_WRONG_FORMAT';
                     }
                 } break;
             }
 
-            return fields;
+            return records;
         }
 
-        static #lines2blocks(lines) {
+        static #records2Blocks(records) {
             const blocks = [];
             let startAddress = 0;
 
-            for (let fields of lines) {
-                switch (fields.recordType) {
+            for (let record of records) {
+                switch (record.type) {
                     case 0: {
-                        blocks.push({
-                            address: startAddress + fields.address,
-                            data: fields.data,
-                        });
+                        if (record.data?.length) {
+                            blocks.push({
+                                address: startAddress + record.address,
+                                data: record.data,
+                            });
+                        }
                     } break;
 
                     case 1: {
@@ -169,13 +259,13 @@ class intelhexCodec {
                     } break;
 
                     case 2: {
-                        startAddress = fields.data[0] * (2 ** 12);
-                        startAddress += fields.data[1] * (2 ** 4);
+                        startAddress = record.data[0] * (2 ** 12);
+                        startAddress += record.data[1] * (2 ** 4);
                     } break;
 
                     case 4: {
-                        startAddress = fields.data[0] * (2 ** 24);
-                        startAddress += fields.data[1] * (2 ** 16);
+                        startAddress = record.data[0] * (2 ** 24);
+                        startAddress += record.data[1] * (2 ** 16);
                     } break;
                 }
             }
@@ -189,10 +279,6 @@ class intelhexCodec {
             for (let block of blocks) {
                 let idx = 0;
 
-                if (newBlocks.length <= 0) {
-                    newBlocks.push(block);
-                    continue;
-                }
                 for (; idx < newBlocks.length; ++idx) {
                     const newBlk = newBlocks[idx];
 
@@ -210,12 +296,18 @@ class intelhexCodec {
                             }
                             newBlocks.splice(idx + 1, 1);
                         }
+                        break;
                     } else if (newBlk.address === (block.address + block.data.length)) {
                         newBlk.data = [...block.data, ...newBlk.data];
                         newBlk.address = block.address;
+                        break;
                     } else if (block.address < newBlk.address) {
                         newBlocks.splice(idx, 0, block);
+                        break;
                     }
+                }
+                if (idx >= newBlocks.length) {
+                    newBlocks.push(block);
                 }
             }
 
@@ -224,12 +316,12 @@ class intelhexCodec {
 
         static fromString = (input) => {
             input = [...input];
-            const fieldsOfLines = [];
+            const records = [];
             const lines = this.#split2Lines(input);
             for (let line of lines) {
-                fieldsOfLines.push(this.#splitLine2fields(line));
+                records.push(this.#splitLine2Records(line));
             }
-            const blocks = this.#lines2blocks(fieldsOfLines);
+            const blocks = this.#records2Blocks(records);
             return this.#mergeBlocks(blocks);
         };
     };
@@ -238,15 +330,3 @@ class intelhexCodec {
 export {
     intelhexCodec,
 };
-
-import fs from 'fs/promises';
-let file = await fs.open('./test.hex');
-let output = await fs.open('./output.txt', 'w');
-
-let data = await file.readFile();
-
-try {
-    const blocks = intelhexCodec.decode.fromString(data);
-} catch (e) {
-    console.log('===:', e)
-}
